249d78bc6f3fcce948c126d381269bf5e2eaf82e
Im-Rik <sourikray2002@gmail.com>
2025-06-08 16:40:21 +0530

    code refactored

--- backend/app/routes/api.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-api_blueprint = Blueprint('api', __name__)
-
-@api_blueprint.route('/hello', methods=['GET'])
-def hello_world():
-    return jsonify({"message": "Hello from Flask backend!"})
